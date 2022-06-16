ffb9305187f7b6926a021f15568252cbd6927c21
Maria Bonete <bonetesalmeron@gmail.com>
2022-06-16 20:12:35 +0200

    finished workshop

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,21 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  small: {
+    fontSize: 14/16,
+    iconSize: 16,
+    borderThickness:1,
+    height: 24/16,  
+  },
+  large: {
+    fontSize: 18/16,
+    iconSize: 24,
+    borderThickness:2,
+    height: 36/16,  
+  },
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +28,62 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
-};
+
+  const styles = STYLES[size];
+  if(!styles) throw new Error('No size is defined')
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{'--size': styles.iconSize + 'px'}}>
+        <Icon id={icon} size={styles.iconSize}/>
+      </IconWrapper>
+      <Input placeholder={placeholder} style={{
+        '--width': width + 'px', 
+        '--height': styles.height + 'rem',
+        '--border-thickness': styles.borderThickness + 'px',
+        '--font-size': styles.fontSize + 'rem',
+        }}/>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`;
+
+const Input = styled.input`
+  width: var(--width);
+  height: var(--heigth);
+  border: none;
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
+  padding-left: var(--height);
+  color: inherit;
+  font-size: var(--font-size);
+  font-weight: 700;
+  outline-offset: 2px;
+
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`;
+
+
+
 
 export default IconInput;
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,65 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { Children } from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+
+  let BarBox;
+  if(size === 'small') {
+    BarBox = SmallBarBox;
+  } else if(size === 'medium'){
+    BarBox = MediumBarBox;
+  } else if(size === 'large'){
+    BarBox = LargeBarBox;
+  } else {
+    throw new Error(`Unrecognized Bar size: ${size}`);
+  }
+
+  return (
+    <BarBox 
+      role="progressbar" 
+      aria-valuenow={value} 
+      aria-valuemin="0"
+      aria-valuemax="100"
+    >
+      <Bar width={value}/>
+      <VisuallyHidden>{value}%</VisuallyHidden>
+    </BarBox>
+  );
 };
+
+const BarBoxBase = styled.div `
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  /* Trims off corners when progress bar is near full */
+  overflow: hidden;
+`;
+
+const Bar = styled.div `
+  height: 100%;
+  width: ${p => p.width + '%'};
+  background-color: ${COLORS.primary};
+  border-radius:${p => p.width === 100 ? '4px' : '4px 0 0 4px'}
+`;
+
+const SmallBarBox = styled(BarBoxBase)`
+  height: 8px;
+  border-radius: 4px;
+`;
+
+const MediumBarBox = styled(BarBoxBase)`
+  height: 12px;
+  border-radius: 4px;
+`;
+
+const LargeBarBox = styled(BarBoxBase)`
+  height: 24px;
+  padding: 4px;
+  border-radius: 8px;
+`;
 
 export default ProgressBar;
